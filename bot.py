e7d3258bef9868d3ff38c6a3ee0e9f26508069d9
Puyodead1 <23562356riley@gmail.com>
2019-11-15 17:01:12 -0500

    Add some comments to bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,24 @@ import sys, traceback, discord, os
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Create a new 'bot' with prefix
 bot = commands.Bot(command_prefix="!", description="PyBot")
 
+# Loads cogs from commands folder, no need to touch this when adding new commands, it loads them automagically!
 if __name__ == '__main__':
     for file in os.listdir("commands"):
         if file.endswith("py"):
             bot.load_extension('commands.{0}'.format(file.split(".")[0]))
 
 
+# Ready event
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}#{bot.user.discriminator}')
     await bot.change_presence(activity=discord.Activity(name='in ' + str(len(bot.guilds)) + " server!", type=discord.ActivityType.playing))
 
 
+# load the .env file with token
 load_dotenv()
+# login to discord
 bot.run(os.getenv("TOKEN"))
