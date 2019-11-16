096ddab6a853f8e1e6fd6716a6db0d3a5fc9f3d7
Puyodead1 <23562356riley@gmail.com>
2019-11-15 19:45:43 -0500

    Updates
    - Added cog management
    - Added event cogs
    - Added command error handler cog
    - Added config
    - Added 'isAdmin' command check function to cogmanagement

--- bot.py
+++ bot.py
@@ -6,18 +6,31 @@ from discord.ext.commands import errors
 from dotenv import load_dotenv
 
 # Create a new 'bot' with prefix
-bot = commands.Bot(command_prefix="!", description="PyBot")
+bot = commands.Bot(command_prefix="{", description="PyBot")
 
-# Loads cogs from commands folder, no need to touch this when adding new commands, it loads them automagically!
+# Loads commands from commands folder, no need to touch this when adding new commands, it loads them automagically!
 if __name__ == '__main__':
-    for file in os.listdir("commands"):
-        if file.endswith("py"):
-            filename = file.split(".")[0]
+    # loads commands
+    for command in os.listdir("commands"):
+        if command.endswith("py"):
+            filename = command.split(".")[0]
             try:
                 bot.load_extension(f"commands.{filename}")
                 print(f"Command Loaded: {filename}")
-            except (errors.ExtensionNotFound, errors.ExtensionAlreadyLoaded, errors.NoEntryPointError, errors.ExtensionFailed) as e:
+            except (errors.ExtensionNotFound, errors.ExtensionAlreadyLoaded, errors.NoEntryPointError,
+                    errors.ExtensionFailed) as e:
                 print(f"Error loading command: {filename}, Error: {e}")
+
+    # loads events
+    for event in os.listdir("events"):
+        if event.endswith("py"):
+            filename = event.split(".")[0]
+            try:
+                bot.load_extension(f"events.{filename}")
+                print(f"Event Loaded: {filename}")
+            except (errors.ExtensionNotFound, errors.ExtensionAlreadyLoaded, errors.NoEntryPointError,
+                    errors.ExtensionFailed) as e:
+                print(f"Error loading event: {filename}, Error: {e}")
 
 
 # Ready event
@@ -25,7 +38,8 @@ if __name__ == '__main__':
 async def on_ready():
     bot.startedAt = time.time()
     print(f'Logged in as {bot.user.name}#{bot.user.discriminator}')
-    await bot.change_presence(activity=discord.Activity(name='in ' + str(len(bot.guilds)) + " server!", type=discord.ActivityType.playing))
+    await bot.change_presence(
+        activity=discord.Activity(name='in ' + str(len(bot.guilds)) + " server!", type=discord.ActivityType.playing))
 
 
 # load the .env file with token
--- commands/cogmanagment.py
+++ commands/cogmanagment.py
@@ -1,5 +1,13 @@
 import discord
+from config import ADMINS
 from discord.ext import commands
+
+
+def isAdmin(ctx):
+    if str(ctx.message.author.id) in ADMINS:
+        return True
+    else:
+        return False
 
 
 class CogManagementCog(commands.Cog):
@@ -8,6 +16,7 @@ class CogManagementCog(commands.Cog):
 
     @commands.command(name="reloadcog")
     @commands.guild_only()
+    @commands.check(isAdmin)
     async def reload(self, ctx, *, cog: str):
         # to reload we need to unload it first
         try:
@@ -16,18 +25,30 @@ class CogManagementCog(commands.Cog):
         except Exception as e:
             await ctx.send(f"Failed to reload cog: {type(e).__name__} - {e}")
         else:
-            await ctx.send(f"Cog reloaded!")
+            await ctx.send(f"Cog Reloaded!")
 
     @commands.command(name="loadcog")
     @commands.guild_only()
+    @commands.check(isAdmin)
     async def load(self, ctx, *, cog: str):
         # try to load cog
         try:
             self.bot.load_extension(f"commands.{cog}")
         except Exception as e:
-            await ctx.send(f"Failed to load cog: {type(e).__name__}")
+            await ctx.send(f"Failed to load cog: {type(e).__name__} - {e}")
         else:
-            await ctx.send(f"Cog loaded!")
+            await ctx.send(f"Cog Loaded!")
+
+    @commands.command(name="unloadcog")
+    @commands.guild_only()
+    @commands.check(isAdmin)
+    async def unload(self, ctx, *, cog: str):
+        try:
+            self.bot.unload_extension(f"commands.{cog}")
+        except Exception as e:
+            await ctx.send(f"Failed to unload cog: {type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"Cog Unloaded!")
 
 
 def setup(bot):
