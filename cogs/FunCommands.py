50e76e3deef2a7d2806547c6c939bb9e7ef9eef2
Puyodead1 <23562356riley@gmail.com>
2019-12-05 12:26:14 -0500

    GNOMED

--- cogs/FunCommands.py
+++ cogs/FunCommands.py
@@ -319,6 +319,18 @@ class FunCommandsCog(commands.Cog):
         embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
         await ctx.send(content=None, embed=embed)
 
+    @commands.command(name="gnome")
+    @commands.guild_only()
+    async def gnomed(self, ctx):
+        embed = discord.Embed(title=None,
+                              description=None,
+                              color=discord.Color.green(), timestamp=datetime.utcnow())
+        embed.set_image(
+            url="https://puyodead1.wtf/u/MEQGPSiVr1.gif")
+        embed.set_footer(text=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+        await ctx.send(content=None, embed=embed)
+
     @commands.command(name="spam", help="Spams a user with 5 mentions", usage="<@user>", hidden=True, enabled=False)
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
