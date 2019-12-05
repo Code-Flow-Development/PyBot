4c44f78cbfb0518b65f356c30474b31ee21e888e
Puyodead1 <23562356riley@gmail.com>
2019-12-04 22:32:30 -0500

    fixed the eval command :joy:

--- cogs/BotAdminCommands.py
+++ cogs/BotAdminCommands.py
@@ -1,3 +1,4 @@
+import ast
 import inspect
 from datetime import datetime
 
@@ -108,34 +109,37 @@ class BotAdminCommandsCog(commands.Cog):
     @commands.command(name="eval", hidden=True, help="Evaluates code")
     @checks.isBotAdmin()
     async def eval(self, ctx, *, code: str):
-        code = code.strip('` ')
+        fn_name = "_eval_expr"
+
+        cmd = code.strip("` ")
+
+        # add a layer of indentation
+        cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+
+        # wrap in async def body
+        body = f"async def {fn_name}():\n{cmd}"
+
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+
+        insert_returns(body)
+
+        env = {
+            'bot': ctx.bot,
+            'discord': discord,
+            'commands': commands,
+            'ctx': ctx,
+            '__import__': __import__
+        }
 
         try:
-            result = eval(code)
-            if inspect.isawaitable(result):
-                result = await result
+            exec(compile(parsed, filename="<ast>", mode="exec"), env)
 
+            result = (await eval(f"{fn_name}()", env))
             if result is not None:
-                await ctx.send(f"```py\n{result}\n```")
+                return await ctx.send(f"```py\n{result}\n```")
             else:
-                await ctx.send("[Eval] Empty result")
-        except Exception as e:
-            await ctx.send(f"[Eval] Error running code: {type(e).__name__} - {e}")
-            
-    @commands.command(name="exec", hidden=True, help="Executes code")
-    @checks.isBotAdmin()
-    async def exec(self, ctx, *, code: str):
-        code = code.strip('` ')
-
-        try:
-            result = exec(code)
-            if inspect.isawaitable(result):
-                result = await result
-
-            if result is not None:
-                await ctx.send(f"```py\n{result}\n```")
-            else:
-                await ctx.send("[Eval] Empty result")
+                return await ctx.send(f"Empty Result")
         except Exception as e:
             await ctx.send(f"[Eval] Error running code: {type(e).__name__} - {e}")
 
@@ -160,7 +164,51 @@ class BotAdminCommandsCog(commands.Cog):
                                   timestamp=datetime.utcnow())
             embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
             embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+            """Evaluates input.
+                Input is interpreted as newline seperated statements.
+                If the last statement is an expression, that is the return value.
+                Usable globals:
+                  - `bot`: the bot instance
+                  - `discord`: the discord module
+                  - `commands`: the discord.ext.commands module
+                  - `ctx`: the invokation context
+                  - `__import__`: the builtin `__import__` function
+                Such that `>eval 1 + 1` gives `2` as the result.
+                The following invokation will cause the bot to send the text '9'
+                to the channel of invokation and return '3' as the result of evaluating
+                >eval ```
+                a = 1 + 2
+                b = a * 2
+                await ctx.send(a + b)
+                a
+                ```
+                """
+            fn_name = "_eval_expr"
 
+            cmd = cmd.strip("` ")
+
+            # add a layer of indentation
+            cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+
+            # wrap in async def body
+            body = f"async def {fn_name}():\n{cmd}"
+
+            parsed = ast.parse(body)
+            body = parsed.body[0].body
+
+            insert_returns(body)
+
+            env = {
+                'bot': ctx.bot,
+                'discord': discord,
+                'commands': commands,
+                'ctx': ctx,
+                '__import__': __import__
+            }
+            exec(compile(parsed, filename="<ast>", mode="exec"), env)
+
+            result = (await eval(f"{fn_name}()", env))
+            await ctx.send(result)
             await ctx.send(content=None, embed=embed)
         else:
             embed = discord.Embed(title="New Bot Admin",
@@ -228,6 +276,22 @@ class BotAdminCommandsCog(commands.Cog):
         except Exception as e:
             await ctx.send(f"[BotAdminCommands] Failed to change presence! Error: {e}")
             getLogger().error(f"[BotAdminCommands] Failed to change presence! Error: {e}")
+
+
+def insert_returns(body):
+    # insert return stmt if the last expression is a expression statement
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    # for if statements, we insert returns into the body and the orelse
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
 
 
 def setup(bot):
